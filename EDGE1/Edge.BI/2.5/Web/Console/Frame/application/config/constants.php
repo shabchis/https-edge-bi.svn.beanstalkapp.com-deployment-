ff6bc24709088dd374350499cbe9667ace338ee6
doronassayas <doronassayas@a0235143-bea1-4189-8641-4830bee2df18>
2011-11-21 16:05:29 +0000

    Added Edge.BI web app

--- EDGE1/Edge.BI/2.5/Web/Console/Frame/application/config/constants.php
+++ EDGE1/Edge.BI/2.5/Web/Console/Frame/application/config/constants.php
@@ -37,11 +37,11 @@ define('FOPEN_WRITE_CREATE_STRICT', 			'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
-define('EDGE_API_URL','http://localhost:6283');
+define('EDGE_API_URL','http://localhost:6283/edgebi');
 $iframeurl = array();
 $iframeurl["Path"] ="";
 
-define('LOGIN_PAGE', "/edge/login.html");
+define('LOGIN_PAGE', "/edge-qa/login.html");
 
 /* End of file constants.php */
 /* Location: ./system/application/config/constants.php */
