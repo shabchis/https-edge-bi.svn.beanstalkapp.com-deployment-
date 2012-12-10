2c99c21d036ddd6a67ca1a06e381db846650e7c0
doronassayas <doronassayas@a0235143-bea1-4189-8641-4830bee2df18>
2012-12-10 16:29:52 +0000

    Adding fb-token tool

--- EDGE1/wwwroot/admin/fb-token/step3.php
+++ EDGE1/wwwroot/admin/fb-token/step3.php
@@ -6,26 +6,47 @@
 	{
 		$token_url = "https://graph.facebook.com/oauth/access_token?" .
 			"client_id=" . $_SESSION['appID'] .
-			"&redirect_uri=" . urlencode(base_url()) .
+			"&redirect_uri=" . urlencode(complete_url()) .
 			"&client_secret=" . $_SESSION['appSecret'] .
 			"&code=" . $_REQUEST["code"] .
 			"&scope=" . $_SESSION['permissions']
 		;
 
-		$response = file_get_contents($token_url);
-		var_dump($response);
+		$response = file_get_contents($token_url,
+			false,
+			stream_context_create(
+				array(
+					'http' => array(
+						'ignore_errors' => true
+					)
+				)
+			));
+		
 		$params = null; parse_str($response, $params);
-		
 		$token = $params['access_token'];
-		$expiresRaw = $params['expires'];
-		$expires = date("Y-m-d H:i:s", time() + intval($expiresRaw));
 		
-		?>
-			<div id="result">
-				Access token <input type="text" readonly="readonly" id="output-token" value="<?php echo $token ?>"/>
-				expires on <span id="output-expires"><?php echo $expires ?></span>
-			</div>
-		<?php
+		if (!isset($token))
+		{
+			?>
+				<div id="error">
+					Error:
+					<br/>
+					<pre><?php echo $response ?></pre>
+				</div>
+			<?php
+		}
+		else
+		{
+			$expiresRaw = $params['expires'];
+			$expires = date("Y-m-d H:i:s", time() + intval($expiresRaw));
+
+			?>
+				<div id="result">
+					Access token <input type="text" readonly="readonly" id="output-token" value="<?php echo $token ?>"/>
+					expires on <span id="output-expires"><?php echo $expires ?></span>
+				</div>
+			<?php
+		}
 	}
 	else {
 		echo("The state does not match. Please try again.");
