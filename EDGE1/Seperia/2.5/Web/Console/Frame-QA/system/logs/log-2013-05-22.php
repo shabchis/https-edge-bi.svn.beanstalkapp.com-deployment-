6e856aabb82e8fbf22a4fbab143e441189b9f803
doronassayas <doronassayas@a0235143-bea1-4189-8641-4830bee2df18>
2013-05-22 16:03:04 +0000

    Publishing updated version of frame

---
+++ EDGE1/Seperia/2.5/Web/Console/Frame-QA/system/logs/log-2013-05-22.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-22 15:25:21 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:27:25 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:27:49 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:28:49 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:31:38 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:32:05 --> 404 Page Not Found --> refund
+ERROR - 2013-05-22 15:32:41 --> 404 Page Not Found --> panorama
+ERROR - 2013-05-22 15:32:42 --> 404 Page Not Found --> panorama
